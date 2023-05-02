dacf01f8b52a4f86d2c3e3eec21f0afde757eaa0
Alejandro Aguirre Soto <aguirre.soto.alejandro@gmail.com>
2023-05-02 17:20:37 -0500

    Add iac to project

---
+++ lambda_triggers.tf
@@ -0,0 +1,17 @@
+resource "aws_cloudwatch_event_rule" "rate_1_minute" {
+  name = "lambda-event-rule-rate-1-min"
+  description = "retry scheduled every 1 min"
+  schedule_expression = "rate(1 minute)"
+}
+
+resource "aws_cloudwatch_event_target" "cosmoagent_trigger" {
+  arn = aws_lambda_function.cosmoagent_lambda.arn
+  rule = aws_cloudwatch_event_rule.rate_1_minute.name
+}
+
+resource "aws_lambda_permission" "allow_eventbridge_cosmoagent" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.cosmoagent_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.rate_1_minute.arn
+}
--- main.tf
+++ main.tf
@@ -1,6 +1,56 @@
+provider "aws" {
+  region = var.region
+}
+
+variable region {
+  type = string
+  default = "sa-east-1"
+}
+
+data "aws_iam_role" "mm_lambda_role" {
+  name = "lambda-mm-basic-role"
+}
+
+data "aws_lambda_layer_version" "binance_layer" {
+  layer_name = "binance-layer"
+}
+
+data "aws_lambda_layer_version" "loguru_layer" {
+  layer_name = "loguru-layer"
+}
+
+module "cosmoagent" {
+  source = "./cosmoagent"
+}
+
+### COSMO AGENT IAC
+
 resource "terraform_data" "cosmoagent_lambda_zip" {
   provisioner "local-exec" {
     command = "zip -r cosmoagent.zip cosmoagent utils"
     interpreter = ["/bin/bash", "-c"]
   }
 }
+
+resource "aws_lambda_function" "cosmoagent_lambda" {
+
+  filename      = "cosmoagent.zip"
+  function_name = "cosmoagent_lambda"
+  role          = data.aws_iam_role.mm_lambda_role.arn
+  handler       = "cosmoagent.cosmoagent.launch"
+  runtime       = "python3.9"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+
+  layers = [ data.aws_lambda_layer_version.binance_layer.arn,
+            data.aws_lambda_layer_version.loguru_layer.arn,
+            "arn:aws:lambda:sa-east-1:336392948345:layer:AWSSDKPandas-Python39:8" ] # AWS Pandas
+
+  depends_on = [ terraform_data.cosmoagent_lambda_zip ]
+}
+
+### COSMO BOT
