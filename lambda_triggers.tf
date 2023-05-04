9abb0e42dfd46d37333721f4c49d5d031e589fae
Alejandro Aguirre Soto <aguirre.soto.alejandro@gmail.com>
2023-05-04 18:05:01 -0500

    Fix syntax tf

--- lambda_triggers.tf
+++ lambda_triggers.tf
@@ -1,11 +1,11 @@
 resource "aws_cloudwatch_event_rule" "rate_1_minute" {
-  name = "lambda-event-rule-rate-1-min"
+  name = "lambda_event_rule_rate_1_min"
   description = "retry scheduled every 1 min"
   schedule_expression = "rate(1 minute)"
 }
 
 resource "aws_cloudwatch_event_target" "cosmoagent_trigger" {
-  target_id = "cosmoagent-event-target"
+  target_id = var.COSMOBOT_DEBUG == "1" ? "cosmoagent_event_lambda_test" : "cosmoagent_event_lambda"
   arn = aws_lambda_function.cosmoagent_lambda.arn
   rule = aws_cloudwatch_event_rule.rate_1_minute.name
 }
@@ -15,5 +15,5 @@ resource "aws_lambda_permission" "allow_eventbridge_cosmoagent" {
   function_name = aws_lambda_function.cosmoagent_lambda.function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.rate_1_minute.arn
-  statement_id  = "event-bridge-trigger-cosmoagent"
+  statement_id  = "event_bridge_trigger_cosmoagent"
 }
