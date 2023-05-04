c7a5912cb8f92bdc25990720f28ffd2b2fd9a856
Alejandro Aguirre Soto <aguirre.soto.alejandro@gmail.com>
2023-05-04 16:32:38 -0500

    Fix some IaC issues

--- lambda_triggers.tf
+++ lambda_triggers.tf
@@ -5,6 +5,7 @@ resource "aws_cloudwatch_event_rule" "rate_1_minute" {
 }
 
 resource "aws_cloudwatch_event_target" "cosmoagent_trigger" {
+  target_id = "cosmoagent-event-target"
   arn = aws_lambda_function.cosmoagent_lambda.arn
   rule = aws_cloudwatch_event_rule.rate_1_minute.name
 }
@@ -14,4 +15,5 @@ resource "aws_lambda_permission" "allow_eventbridge_cosmoagent" {
   function_name = aws_lambda_function.cosmoagent_lambda.function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.rate_1_minute.arn
+  statement_id  = "event-bridge-trigger-cosmoagent"
 }
