399130f44c41b5b240ec33aa83e311a754da6ad3
Alejandro Aguirre Soto <aguirre.soto.alejandro@gmail.com>
2023-05-31 17:00:11 -0500

    Change rate time for cosmoagent. less aggregation of data

--- lambda_triggers.tf
+++ lambda_triggers.tf
@@ -5,6 +5,12 @@ resource "aws_cloudwatch_event_rule" "rate_1_minute" {
   schedule_expression = "rate(1 minute)"
 }
 
+resource "aws_cloudwatch_event_rule" "rate_2_minutes" {
+  name = "lambda_event_rule_rate_2_min"
+  description = "retry scheduled every 2 min"
+  schedule_expression = "rate(2 minute)"
+}
+
 resource "aws_cloudwatch_event_rule" "rate_8_minutes" {
   name = "lambda_event_rule_rate_8_min"
   description = "retry scheduled every 8 min"
@@ -22,14 +28,14 @@ resource "aws_cloudwatch_event_rule" "rate_20_minutes" {
 resource "aws_cloudwatch_event_target" "cosmoagent_crypto_trigger" {
   target_id = var.STAGING == "1" ? "cosmoagent_crypto_event_lambda_staging" : "cosmoagent_crypto_event_lambda"
   arn = aws_lambda_function.cosmoagent_crypto_lambda.arn
-  rule = aws_cloudwatch_event_rule.rate_1_minute.name
+  rule = aws_cloudwatch_event_rule.rate_2_minutes.name
 }
 
 resource "aws_lambda_permission" "allow_eventbridge_cosmoagent_crypto" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.cosmoagent_crypto_lambda.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.rate_1_minute.arn
+  source_arn    = aws_cloudwatch_event_rule.rate_2_minutes.arn
   statement_id  = "event_bridge_trigger_cosmoagent_crypto"
 }
 
